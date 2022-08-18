8300dc97105e17bdde298f177656554143ba3f76
Abdallah Mahmoud Ahmed Mahmoud Attallah <Abdallah.Attallah@Egypt.TE-Data.core>
2022-08-18 12:27:29 +0200

    Handle JS DataTypes

---
+++ VariablesAndDataTypes/dataTypes.js
@@ -0,0 +1,34 @@
+// DataTypes 
+// Describe the different types or kinds of data.
+// The set of types in JS consists of primitive values and objects.
+// note : Primitive types already defined in our language.
+// Js Variables can hold different data types;
+
+// Primitive Type
+// String
+let str="Hello, World!";
+console.log(str);
+// Boolean
+let bool=true;
+console.log(bool);
+// Number
+let num=14.2;
+console.log(num);
+// Null
+let n =null;
+var s;
+console.log(s);
+// Undefiend 
+let u =undefined;
+console.log(u);
+
+// Q: What's the difference between null and undefiend?
+//      null      => is a variable that is defiened but is missing a value or represent empty.
+//      undefiend => is a variable that refers to something doesn't exist, and the variable isn't defiend to be anything
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Non-primitive type
+let obj={
+    firstName:"abdallah",
+    lastName:"attallah"
+}
