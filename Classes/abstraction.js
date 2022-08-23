d7b1bbd52324db35cdc95d04033fdb5310f73703
Abdallah Attallah <abdallahattallah197@gmail.com>
2022-08-23 10:22:01 +0200

    Test file

---
+++ Classes/abstraction.js
@@ -0,0 +1,3 @@
+// 3-Abstraction
+// Is a way of hiding the implementaions details and showing only the functionality to the users.
+// enhances the understandability and maintainability of the code we write and reduces the duplication of the code.
---
+++ Classes/polymorphism.js
@@ -0,0 +1,2 @@
+// Polymorphism 
+// Is a core concept of OOP that provides a way to perform a single action in a different ways.
